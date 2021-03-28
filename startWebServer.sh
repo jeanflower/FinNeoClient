71cfa90491e71745b7fbfa6da4274c5327be20c3
Jean Flower <jeanflower@rocketmail.com>
2021-03-28 12:04:13 +0100

    server location

--- startWebServer.sh
+++ startWebServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-REACT_APP_ORIGIN_APPENDAGE=/FinNeoClient REACT_APP_SERVER_URL_NOT_SECRET=https://desolate-refuge-81883.herokuapp.com/finneo/ npm start
+REACT_APP_ORIGIN_APPENDAGE=/FinNeoClient REACT_APP_SERVER_URL_NOT_SECRET=https://desolate-island-97738.herokuapp.com/finneo/ npm start
